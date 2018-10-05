4bacd525445f551d4bb87c1dd68e5291e410240e
Pavlo Marchuk <pavlo_marchuk@hotmail.com>
2018-10-05 06:31:27 +0300

    Add HomeControllerTests

---
+++ CrossMatrix.Tests.Web/HomeControllerTests/HomeControllerSut.cs
@@ -0,0 +1,19 @@
+﻿using CrossMatrix.Model.Services.Abstract;
+using CrossMatrix.Web.Controllers;
+using Moq;
+
+namespace CrossMatrix.Tests.Web.HomeControllerTests
+{
+	public class HomeControllerSut
+	{
+		public HomeController Instance { get; set; }
+		public Mock<IMatrixService> MockMatrixService { get; set; }
+
+		public HomeControllerSut()
+		{
+			MockMatrixService = new Mock<IMatrixService>();
+
+			Instance = new HomeController(MockMatrixService.Object);
+		}
+	}
+}
---
+++ CrossMatrix.Tests.Web/HomeControllerTests/IndexMethodShould.cs
@@ -0,0 +1,53 @@
+﻿using CrossMatrix.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+//HomeControllerSut
+namespace CrossMatrix.Tests.Web.HomeControllerTests
+{
+	[TestClass]
+	public class IndexMethodShould
+	{
+		public HomeControllerSut Sut { get; set; }
+
+		public IndexMethodShould()
+		{
+			Sut = new HomeControllerSut();
+		}
+
+		[TestMethod]
+		public void ReturnViewResultWhenModelIsNull()
+		{
+			// Arrange
+
+			Sut.MockMatrixService.Setup(x => x.GetNumberOfPluses("010{0}111{0}010")).Returns(1);
+
+			// Action
+			IActionResult result = Sut.Instance.Index(null) as IActionResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void ReturnViewResultWhenModelIsValid()
+		{
+			// Arrange
+			MatrixModel model = new MatrixModel
+			{
+				MatrixString = string.Format("010{0}111{0}010", Environment.NewLine),
+				PlusesСounter = 0,
+				InvalidFeedback = ""
+			};
+
+			Sut.MockMatrixService.Setup(x => x.GetNumberOfPluses(model.MatrixString)).Returns(1);
+
+			// Action
+			IActionResult result = Sut.Instance.Index(null) as IActionResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+	}
+}
